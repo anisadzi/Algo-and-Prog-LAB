6808125eba30350983601ff39b4e747ec1e8520e
niverred <anisa.zikra@gmail.com>
2021-10-26 17:45:01 +0700

    Initial commit

---
+++ Functions_Prog_Exc/Convert to days.py
@@ -0,0 +1,16 @@
+def convert_to_days() :
+    hours = eval(input("Enter number of hours : "))
+    minutes = eval(input("Enter number of minutes : "))
+    seconds = eval(input("Enter number of seconds : "))
+    return hours, minutes, seconds
+
+def get_days(hours, minutes, seconds):
+    days = (hours / 24) + (minutes / 1440) + (seconds / 86400)
+    print(f"{days:.4f}")
+
+def main():
+    hours, minutes, seconds = convert_to_days()
+    get_days(hours, minutes, seconds)
+
+
+main()
